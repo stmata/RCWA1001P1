c4600146a44a878f3a1950aa4231fb0c54eb24b2
stmata <108939392+stmata@users.noreply.github.com>
2024-09-30 11:00:58 -0400

    Routes defition
    Added 'home' rounte in main.py. Completed base.html and home.html

---
+++ fastapi_app/main.py
@@ -0,0 +1,29 @@
+import sys, os
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.templating import Jinja2Templates
+
+# Add the root directory to the syspath
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Create FastAPI app object
+app = FastAPI()
+
+# Get the absolute path to the templates directory
+templates_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
+# Get the absolute path to the static directory
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "static"))
+# Serve static files
+app.mount("static", StaticFiles(directory=static_dir))
+# Set up Jinja2 template for rendering HTML files
+templates = Jinja2Templates(directory=templates_dir)
+
+# In-memory user storage for login
+users = {"admin": "password"}
+
+# Define routes
+@app.get("/")
+async def home_page(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
