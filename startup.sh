d93557a52626e8e679a0477c6e044db6e6f8a51d
stmata <108939392+stmata@users.noreply.github.com>
2024-09-30 13:39:31 -0400

    Added startup.sh file
    We define the execution script into startup.sh file.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Move into the correct directory where the FastAPI app is located
-cd RCWA1001P1
+
 
 # Run the FastAPI + Dash app using Uvicorn
 uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8002 --workers 1
